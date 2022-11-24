ab9cd7a3675cc3ae4e0be3e9ccd5cdf6668c2bd4
OdiousCode <gorgande@gmail.com>
2022-11-24 16:25:59 +0100

    it works

---
+++ src/data/items.js
@@ -0,0 +1,36 @@
+const items = [
+  {
+    id: 1,
+    name: "Comfy slippers",
+    price: 12.0,
+    category: "Footwear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, iste inventore reprehenderit eos ratione tenetur eveniet iusto suscipit consequatur architecto accusantium aliquam vero quaerat repellendus explicabo possimus officiis exercitationem nulla.",
+  },
+  {
+    id: 2,
+    name: "Tie",
+    price: 5.0,
+    category: "Clothing accessory",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, iste inventore reprehenderit eos ratione tenetur eveniet iusto suscipit consequatur architecto accusantium aliquam vero quaerat repellendus explicabo possimus officiis exercitationem nulla.",
+  },
+  {
+    id: 3,
+    name: "Hat",
+    price: 112.0,
+    category: "Headwear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, iste inventore reprehenderit eos ratione tenetur eveniet iusto suscipit consequatur architecto accusantium aliquam vero quaerat repellendus explicabo possimus officiis exercitationem nulla.",
+  },
+  {
+    id: 4,
+    name: "Sneakers",
+    price: 72.0,
+    category: "Footwear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, iste inventore reprehenderit eos ratione tenetur eveniet iusto suscipit consequatur architecto accusantium aliquam vero quaerat repellendus explicabo possimus officiis exercitationem nulla.",
+  },
+];
+
+export default items;
---
+++ src/views/Cart.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Your Cart</h1>
+        <!-- <div v-for="product in items" :key="product.id">{{ product.name }}</div> -->
+        <CartItemCard v-for="product in products" :key="product.id" :product="product" />
+        <CartSummaryPaymentCard />
+
+    </div>
+</template>
+
+<script>
+import CartItemCard from '../components/cart/CartItemCard.vue'
+import CartSummaryPaymentCard from '../components/cart/CartSummaryPaymentCard.vue'
+
+
+export default {
+    components: {
+        CartItemCard, CartSummaryPaymentCard
+    },
+    computed: {
+        products() {
+            return this.$store.getters.cartItems
+        }
+    }
+}
+</script>
+
